5f7b30e252f353c13896f181e803f7a358b496de
disaacjm <disaacjm@gmail.com>
2023-06-20 11:51:47 +0100

    Initial setup

---
+++ src/components/Footer.js
@@ -0,0 +1,6 @@
+function Footer (){
+    return(
+<h1>This is the footer</h1>
+)}
+
+export default Footer
---
+++ src/components/Main.js
@@ -0,0 +1,6 @@
+function Main(){
+    return(
+<h1>This is the main</h1>
+)} 
+
+export default Main
